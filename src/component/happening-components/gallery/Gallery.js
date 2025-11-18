395cb2904c839e28058b2deb9fe5ecdf8d7c6cd3
Anil Singh <anilstercodigitex@gmail.com>
2025-11-18 12:55:51 +0530

    gallery data comes from api now

--- src/component/happening-components/gallery/Gallery.js
+++ src/component/happening-components/gallery/Gallery.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./gallery.module.css";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -13,236 +13,256 @@ import Link from "next/link";
 import { FaChevronRight, FaChevronLeft, FaChevronDown } from "react-icons/fa6";
 import { CiImageOn } from "react-icons/ci";
 import { PiVideoCameraLight } from "react-icons/pi";
+
+const BASE_URL = "http://sd7:8080/jss/api/happenings/gallery";
+
 export default function Gallery() {
-  const upCommingEvents = [
-    {
-      id: 1,
-      title: "That Shake The Future",
-      banner_image:
-        "https://project-demo.in/jss/assets/img/happenings/banners/1760524600_68ef793871375.png",
-      event_type: "Event",
-      event_date_from: "2023-09-01",
-    },
-    {
-      id: 2,
-      title: "Future That Shake ",
-      banner_image:
-        "https://project-demo.in/jss/assets/img/happenings/banners/1760524600_68ef793871375.png",
-      event_type: "Event",
-      event_date_from: "2023-06-07",
-    },
-  ];
-  const galleryData = [
-    {
-      id: 1,
-      title:
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
-      date: "February 12, 2018",
-      stats: {
-        photos: 48,
-        videos: 12,
-      },
-      thumbnail:
-        "https://project-demo.in/jss/assets/img/happenings/banners/1760523633_68ef75715a3a1.png",
-      media: [
-        {
-          type: "image",
-          url: "/images/home-page/gallary-popup-dummy-banner.png",
-          alt: "Gallery Image 1",
-        },
-        {
-          type: "video",
-          url: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-          alt: "Gallery Video 1",
-        },
-        {
-          type: "image",
-          url: "/images/home-page/gallary-popup-dummy-banner.png",
-          alt: "Gallery Image 2",
-        },
-        {
-          type: "image",
-          url: "/images/home-page/gallary-popup-dummy-banner.png",
-          alt: "Gallery Image 3",
-        },
-      ],
-    },
-    {
-      id: 2,
-      title:
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
-      date: "February 12, 2018",
-      stats: {
-        photos: 32,
-        videos: 8,
-      },
-      thumbnail: "/images/home-page/fifth-section-banner.png",
-      media: [
-        {
-          type: "image",
-          url: "/images/home-page/gallary-popup-dummy-banner.png",
-          alt: "Gallery Image 4",
-        },
-        {
-          type: "image",
-          url: "/images/home-page/gallary-popup-dummy-banner.png",
-          alt: "Gallery Image 5",
-        },
-        {
-          type: "video",
-          url: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-          alt: "Gallery Video 2",
-        },
-      ],
-    },
-    {
-      id: 3,
-      title:
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
-      date: "February 12, 2018",
-      stats: {
-        photos: 56,
-        videos: 15,
-      },
-      thumbnail:
-        "https://project-demo.in/jss/assets/img/happenings/banners/1760523633_68ef75715a3a1.png",
-      media: [
-        {
-          type: "video",
-          url: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-          alt: "Gallery Video 3",
-        },
-        {
-          type: "image",
-          url: "/images/home-page/gallary-popup-dummy-banner.png",
-          alt: "Gallery Image 6",
-        },
-      ],
-    },
-    {
-      id: 4,
-      title:
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
-      date: "February 12, 2018",
-      stats: {
-        photos: 24,
-        videos: 6,
-      },
-      thumbnail:
-        "https://project-demo.in/jss/assets/img/happenings/banners/1760523633_68ef75715a3a1.png",
-      media: [
-        {
-          type: "image",
-          url: "/images/home-page/gallary-popup-dummy-banner.png",
-          alt: "Gallery Image 7",
-        },
-        {
-          type: "image",
-          url: "/images/home-page/gallary-popup-dummy-banner.png",
-          alt: "Gallery Image 8",
-        },
-      ],
-    },
-    {
-      id: 5,
-      title:
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
-      date: "February 12, 2018",
-      stats: {
-        photos: 40,
-        videos: 10,
-      },
-      thumbnail:
-        "https://project-demo.in/jss/assets/img/happenings/banners/1760523633_68ef75715a3a1.png",
-      media: [
-        {
-          type: "image",
-          url: "/images/home-page/gallary-popup-dummy-banner.png",
-          alt: "Gallery Image 9",
-        },
-      ],
-    },
-    {
-      id: 6,
-      title:
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
-      date: "February 12, 2018",
-      stats: {
-        photos: 64,
-        videos: 18,
-      },
-      thumbnail:
-        "https://project-demo.in/jss/assets/img/happenings/banners/1760523633_68ef75715a3a1.png",
-      media: [
-        {
-          type: "video",
-          url: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-          alt: "Gallery Video 4",
-        },
-      ],
-    },
-    {
-      id: 7,
-      title:
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
-      date: "February 12, 2018",
-      stats: {
-        photos: 36,
-        videos: 9,
-      },
-      thumbnail:
-        "https://project-demo.in/jss/assets/img/happenings/banners/1760523633_68ef75715a3a1.png",
-      media: [
-        {
-          type: "image",
-          url: "/images/home-page/gallary-popup-dummy-banner.png",
-          alt: "Gallery Image 10",
-        },
-      ],
-    },
-    {
-      id: 8,
-      title:
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
-      date: "February 12, 2018",
-      stats: {
-        photos: 28,
-        videos: 7,
-      },
-      thumbnail:
-        "https://project-demo.in/jss/assets/img/happenings/banners/1760523633_68ef75715a3a1.png",
-      media: [
-        {
-          type: "image",
-          url: "/images/home-page/gallary-popup-dummy-banner.png",
-          alt: "Gallery Image 11",
-        },
-      ],
-    },
-    {
-      id: 9,
-      title:
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
-      date: "February 12, 2018",
-      stats: {
-        photos: 52,
-        videos: 14,
-      },
-      thumbnail:
-        "https://project-demo.in/jss/assets/img/happenings/banners/1760523633_68ef75715a3a1.png",
-      media: [
-        {
-          type: "image",
-          url: "/images/home-page/gallary-popup-dummy-banner.png",
-          alt: "Gallery Image 12",
-        },
-      ],
-    },
-  ];
+  // const upCommingEvents = [
+  //   {
+  //     id: 1,
+  //     title: "That Shake The Future",
+  //     banner_image:
+  //       "https://project-demo.in/jss/assets/img/happenings/banners/1760524600_68ef793871375.png",
+  //     event_type: "Event",
+  //     event_date_from: "2023-09-01",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Future That Shake ",
+  //     banner_image:
+  //       "https://project-demo.in/jss/assets/img/happenings/banners/1760524600_68ef793871375.png",
+  //     event_type: "Event",
+  //     event_date_from: "2023-06-07",
+  //   },
+  // ];
+  // const galleryData = [
+  //   {
+  //     id: 1,
+  //     title:
+  //       "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
+  //     date: "February 12, 2018",
+  //     stats: {
+  //       photos: 48,
+  //       videos: 12,
+  //     },
+  //     thumbnail:
+  //       "https://project-demo.in/jss/assets/img/happenings/banners/1760523633_68ef75715a3a1.png",
+  //     media: [
+  //       {
+  //         type: "image",
+  //         url: "/images/home-page/gallary-popup-dummy-banner.png",
+  //         alt: "Gallery Image 1",
+  //       },
+  //       {
+  //         type: "video",
+  //         url: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+  //         alt: "Gallery Video 1",
+  //       },
+  //       {
+  //         type: "image",
+  //         url: "/images/home-page/gallary-popup-dummy-banner.png",
+  //         alt: "Gallery Image 2",
+  //       },
+  //       {
+  //         type: "image",
+  //         url: "/images/home-page/gallary-popup-dummy-banner.png",
+  //         alt: "Gallery Image 3",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 2,
+  //     title:
+  //       "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
+  //     date: "February 12, 2018",
+  //     stats: {
+  //       photos: 32,
+  //       videos: 8,
+  //     },
+  //     thumbnail: "/images/home-page/fifth-section-banner.png",
+  //     media: [
+  //       {
+  //         type: "image",
+  //         url: "/images/home-page/gallary-popup-dummy-banner.png",
+  //         alt: "Gallery Image 4",
+  //       },
+  //       {
+  //         type: "image",
+  //         url: "/images/home-page/gallary-popup-dummy-banner.png",
+  //         alt: "Gallery Image 5",
+  //       },
+  //       {
+  //         type: "video",
+  //         url: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+  //         alt: "Gallery Video 2",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 3,
+  //     title:
+  //       "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
+  //     date: "February 12, 2018",
+  //     stats: {
+  //       photos: 56,
+  //       videos: 15,
+  //     },
+  //     thumbnail:
+  //       "https://project-demo.in/jss/assets/img/happenings/banners/1760523633_68ef75715a3a1.png",
+  //     media: [
+  //       {
+  //         type: "video",
+  //         url: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+  //         alt: "Gallery Video 3",
+  //       },
+  //       {
+  //         type: "image",
+  //         url: "/images/home-page/gallary-popup-dummy-banner.png",
+  //         alt: "Gallery Image 6",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 4,
+  //     title:
+  //       "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
+  //     date: "February 12, 2018",
+  //     stats: {
+  //       photos: 24,
+  //       videos: 6,
+  //     },
+  //     thumbnail:
+  //       "https://project-demo.in/jss/assets/img/happenings/banners/1760523633_68ef75715a3a1.png",
+  //     media: [
+  //       {
+  //         type: "image",
+  //         url: "/images/home-page/gallary-popup-dummy-banner.png",
+  //         alt: "Gallery Image 7",
+  //       },
+  //       {
+  //         type: "image",
+  //         url: "/images/home-page/gallary-popup-dummy-banner.png",
+  //         alt: "Gallery Image 8",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 5,
+  //     title:
+  //       "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
+  //     date: "February 12, 2018",
+  //     stats: {
+  //       photos: 40,
+  //       videos: 10,
+  //     },
+  //     thumbnail:
+  //       "https://project-demo.in/jss/assets/img/happenings/banners/1760523633_68ef75715a3a1.png",
+  //     media: [
+  //       {
+  //         type: "image",
+  //         url: "/images/home-page/gallary-popup-dummy-banner.png",
+  //         alt: "Gallery Image 9",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 6,
+  //     title:
+  //       "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
+  //     date: "February 12, 2018",
+  //     stats: {
+  //       photos: 64,
+  //       videos: 18,
+  //     },
+  //     thumbnail:
+  //       "https://project-demo.in/jss/assets/img/happenings/banners/1760523633_68ef75715a3a1.png",
+  //     media: [
+  //       {
+  //         type: "video",
+  //         url: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+  //         alt: "Gallery Video 4",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 7,
+  //     title:
+  //       "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
+  //     date: "February 12, 2018",
+  //     stats: {
+  //       photos: 36,
+  //       videos: 9,
+  //     },
+  //     thumbnail:
+  //       "https://project-demo.in/jss/assets/img/happenings/banners/1760523633_68ef75715a3a1.png",
+  //     media: [
+  //       {
+  //         type: "image",
+  //         url: "/images/home-page/gallary-popup-dummy-banner.png",
+  //         alt: "Gallery Image 10",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 8,
+  //     title:
+  //       "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
+  //     date: "February 12, 2018",
+  //     stats: {
+  //       photos: 28,
+  //       videos: 7,
+  //     },
+  //     thumbnail:
+  //       "https://project-demo.in/jss/assets/img/happenings/banners/1760523633_68ef75715a3a1.png",
+  //     media: [
+  //       {
+  //         type: "image",
+  //         url: "/images/home-page/gallary-popup-dummy-banner.png",
+  //         alt: "Gallery Image 11",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 9,
+  //     title:
+  //       "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
+  //     date: "February 12, 2018",
+  //     stats: {
+  //       photos: 52,
+  //       videos: 14,
+  //     },
+  //     thumbnail:
+  //       "https://project-demo.in/jss/assets/img/happenings/banners/1760523633_68ef75715a3a1.png",
+  //     media: [
+  //       {
+  //         type: "image",
+  //         url: "/images/home-page/gallary-popup-dummy-banner.png",
+  //         alt: "Gallery Image 12",
+  //       },
+  //     ],
+  //   },
+  // ];
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedGallery, setSelectedGallery] = useState(null);
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+  const [allGallaryData, setAllGallaryData] = useState([]);
+
+  useEffect(() => {
+    const fetchContactData = async () => {
+      try {
+        const res = await fetch(BASE_URL);
+        const data = await res.json();
+        setAllGallaryData(data);
+      } catch (err) {
+        console.error(err);
+        setAllGallaryData([]);
+      }
+    };
+    fetchContactData();
+  }, []);
+  const upCommingEvents = allGallaryData.upcoming_events || [];
+  const galleryData = allGallaryData.gallery_data || [];
 
   const openModal = (gallery) => {
     setSelectedGallery(gallery);
@@ -294,7 +314,7 @@ export default function Gallery() {
   return (
     <section className={styles.mediaSection}>
       <div className={styles.bannerWrapper}>
-        {upCommingEvents.length > 0 ? (
+        {upCommingEvents && upCommingEvents.length > 0 ? (
           <Swiper
             modules={[Navigation, SwiperPagination, Autoplay]}
             navigation={{
@@ -356,33 +376,38 @@ export default function Gallery() {
         </div>
       </div>
       <div className={styles.galleryGrid}>
-        {galleryData.map((item) => (
-          <div
-            key={item.id}
-            className={styles.galleryCard}
-            onClick={() => openModal(item)}
-          >
-            <div className={styles.cardImage}>
-              <div className={styles.imagePlaceholder}>
-                <Image
-                  src={item.thumbnail}
-                  alt="Gallery Thumbnail"
-                  layout="responsive"
-                  width={1200}
-                  height={400}
-                  style={{ width: "100%", height: "auto" }}
-                />
+        {galleryData &&
+          galleryData.map((item) => (
+            <div
+              key={item.id}
+              className={styles.galleryCard}
+              onClick={() => openModal(item)}
+            >
+              <div className={styles.cardImage}>
+                <div className={styles.imagePlaceholder}>
+                  {item.thumbnail && (
+                    <Image
+                      src={item.thumbnail}
+                      alt="Gallery Thumbnail"
+                      layout="responsive"
+                      width={1200}
+                      height={400}
+                      style={{ width: "100%", height: "auto" }}
+                    />
+                  )}
+                </div>
+                {item.stats && (
+                  <div className={styles.cardBadge}>
+                    {item.stats.photos} PHOTOS {item.stats.videos} VIDEOS
+                  </div>
+                )}
               </div>
-              <div className={styles.cardBadge}>
-                {item.stats.photos} PHOTOS {item.stats.videos} VIDEOS
+              <div className={styles.cardContent}>
+                <h3 className={styles.cardTitle}>{item.title}</h3>
+                <p className={styles.cardDate}>{formatDate(item.date)}</p>
               </div>
             </div>
-            <div className={styles.cardContent}>
-              <h3 className={styles.cardTitle}>{item.title}</h3>
-              <p className={styles.cardDate}>{item.date}</p>
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
 
       {/* Modal */}
@@ -437,7 +462,9 @@ export default function Gallery() {
                 {currentSlideIndex + 1} / {selectedGallery.media.length}
               </div>
               <div>
-                <p className={styles.modalDate}>{selectedGallery.date}</p>
+                <p className={styles.modalDate}>
+                  {formatDate(selectedGallery.date)}
+                </p>
                 <h2 className={styles.modalTitle}>{selectedGallery.title}</h2>
               </div>
               <div className={`d-flex gap-2`}>
